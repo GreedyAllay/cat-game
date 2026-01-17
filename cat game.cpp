c8067f09c3d5c87fc8f3364826975e0fa31536f2
Greedy Allay <147523537+GreedyAllay@users.noreply.github.com>
2026-01-17 16:26:28 +0100

    HOLY SHIT A SOMEWHAT WORKING ISH PLAYFORMER THAT IS SUPER BUGGY BUT HEY AT LEAST I MADE IT MYSELF :DDDDD

--- cat game.cpp
+++ cat game.cpp
@@ -6,14 +6,12 @@
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-std::vector<std::vector<int>> stage;
-
 float cameraX = 0;
 float cameraY = 0;
 
 float gravity = 9.81;
 
-std::vector<std::vector<int>> objects; //x, y, w, h, physics
+std::vector<std::vector<int>> objects; //0 x, 1 y, 2 w, 3 h, 4 physics, 5 xv, 6 yv, 7 onfloor
 
 std::vector<int> playerHitbox = {50, 50}; //w, h
 
@@ -49,10 +47,12 @@ bool collide(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
     drawObject(w2 + x2, y2, 10, 10); //right top corner
     drawObject(x2, y2 + h2, 10, 10); //left bottom corner 
     drawObject(x2 + w2, y2 + h2, 10, 10); //right bottom corner
-#endif
 
     drawObject(x1, y1, w1, h1);
     drawObject(x2, y2, w2, h2);
+#endif
+
+
 
     //its so smoll yet so incredibly powerful and painful to do holy crap this took forever
     if (x1 < w2 + x2 &&
@@ -66,14 +66,40 @@ bool collide(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
 }
 
 bool checkCollisions(int id) {
-    for (int i = 0; i < stage.size(); i++) {
+    for (int i = 0; i < objects.size(); i++) {
         if (id == i) continue;
-        if (collide(stage[id][0], stage[id][1], stage[id][2], stage[id][3], stage[i][0], stage[i][1], stage[i][2], stage[i][3])) {
+        if (collide(objects[id][0], objects[id][1], objects[id][2], objects[id][3], objects[i][0], objects[i][1], objects[i][2], objects[i][3])) {
             return true;
         }
     }
     return false;
 }
+
+void moveObject(int id, int dx, int dy) {
+    float stepX = (dx < 0 ? -1 : 1);
+    float stepY = (dy < 0 ? -1 : 1);
+    for (int i = 0; i < abs(dx); i++) {
+        if (checkCollisions(id)) {
+            objects[id][0] -= stepX;
+            break;
+        }
+        else {
+            objects[id][0] += stepX;
+        }
+    }
+    for (int i = 0; i < abs(dy); i++) {
+        if (checkCollisions(id)) {
+            objects[id][1] -= stepY;
+            break;
+        }
+        else {
+            objects[id][1] += stepY;
+        }
+    }
+}
+
+void setXvel(int id, int xv) { objects[id][5] = xv; }
+void setYvel(int id, int yv) { objects[id][6] = yv; }
 
 void handleControls() {
     if (freecam) {
@@ -92,18 +118,16 @@ void handleControls() {
     }
     else {
         if (IsKeyDown(KEY_D)) {
-            if (!checkCollisions(0)) {
-                playerX += 10;
-            }
+            setXvel(0, 10);
         }
         if (IsKeyDown(KEY_A)) {
-            playerX -= 10;
+            setXvel(0, -10);
         }
         if (IsKeyDown(KEY_W)) {
-            playerY -= 10;
+            setYvel(0, -10);
         }
         if (IsKeyDown(KEY_S)) {
-            playerY += 10;
+            moveObject(0, 0, 10);
         }
     }
     if (IsKeyPressed(KEY_LEFT_SHIFT)) {
@@ -112,55 +136,45 @@ void handleControls() {
 
 }
 
-void handleCollisions() {
-    for (int i = 0; i < stage.size(); i++) {
-#if 0
-        std::cout << "iteration " << i << "\n";
-
-        std::cout << stage[i][0] << "\n";
-        std::cout << stage[i][1] << "\n";
-        std::cout << stage[i][2] << "\n";
-        std::cout << stage[i][3] << "\n";
-        std::cout << "-----\n";
-#endif
-
-    }
-}
-
-void defineObject(int x, int y, int w, int h) {
-    stage.push_back({ x, y, w, h });
+void defineObject(int x, int y, int w, int h, bool physics) {
+    objects.push_back({ x, y, w, h, physics, 0, 0, 0 });
 }
 
 void modObject(int id, int x, int y, int w, int h) {
-    stage[id][0] = x;
-    stage[id][1] = y;
-    stage[id][2] = w;
-    stage[id][3] = h;
+    objects[id][0] = x;
+    objects[id][1] = y;
+    objects[id][2] = w;
+    objects[id][3] = h;
 }
 
-void handleMovement() {
-    while (checkCollisions(0)) {
-        playerX -= 1;
+void handlePhysics() {
+    for (int i = 0; i < objects.size(); i++) {
+        if (!objects[i][4]) continue;
+        moveObject(i, objects[i][5], objects[i][6]);
+        int onfloor = 0;
+        objects[i][5] /= 1.5;
+        objects[i][6] += 1;
     }
 }
 
+
+
 int main(void)
 {
-
-
     InitWindow(screenWidth, screenHeight, "cat game");
 
     SetTargetFPS(60);
 
-    defineObject(GetMouseX(), GetMouseY(), 50, 50);
-    defineObject(100, 100, 200, 200);
+    defineObject(100, 50, playerHitbox[0], playerHitbox[1], 1);
+    defineObject(100, 100, 200, 200, 0);
+    defineObject(200, 150, 200, 200, 0);
     ClearBackground(RAYWHITE);
 
     while (!WindowShouldClose())
     {
         handleControls();
-        handleCollisions();
         handleObjects();
+        handlePhysics();
 
         if (checkCollisions(0)) {
             ClearBackground(GREEN);
@@ -177,7 +191,7 @@ int main(void)
         //drawObject(0, 0, 20, 20);
 
         //render player
-        modObject(0, playerX, playerY, playerHitbox[0], playerHitbox[1]);
+        //modObject(0, playerX, playerY, playerHitbox[0], playerHitbox[1]);
 
 
 
